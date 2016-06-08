9151424579bf3ac19348a5c90722dd897ffaf0ff
TCSUnited <UnitedTCS@uniteds-imac.local>
2016-06-08 21:32:20 +0530

    Initial Commit

---
+++ GitExistingProject1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExistingProject1
+//
+//  Created by United TCS on 6/8/16.
+//  Copyright © 2016 United TCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
